ac8a4547ffd1031e64b8813178411db13f704960
Skiro1 <135732349+Skiro1@users.noreply.github.com>
2026-02-03 01:06:30 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Telegram API Configuration / Конфигурация Telegram API
 # Get them from / Получите их на https://my.telegram.org
-API_ID = 20396198
-API_HASH = "283d3d3cc508aa60070b2b1957937a92"
+API_ID = 000
+API_HASH = ""
 
 # Bot Token / Токен бота
 # Get it from / Получите его у @BotFather
-BOT_TOKEN = "8375402837:AAHITEUZWgHNDLOV2ViHNO6IZrZgUYuprZE"
+BOT_TOKEN = ""
 
 # List of user IDs allowed to use the bot (optional)
 # If left empty, the bot will be available to everyone
@@ -13,3 +13,4 @@ BOT_TOKEN = "8375402837:AAHITEUZWgHNDLOV2ViHNO6IZrZgUYuprZE"
 # Список ID пользователей, которым разрешено использовать бота (необязательно)
 # Если оставить пустым, бот будет доступен всем
 ALLOWED_USERS = []
+
